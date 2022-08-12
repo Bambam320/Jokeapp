1aed03b7b4fef41172b0e1f601ec7650523778bb
Igor Mestechkin <Igor.mestechkin@gmail.com>
2022-08-12 11:32:11 -0400

    Cupdated index.js

--- src/index.js
+++ src/index.js
@@ -2,22 +2,46 @@ document.querySelector('#main-search').addEventListener('click', (e) => {
   e.preventDefault()
   //console.log(e)
 
-  const jokeServer = 'https://v2.jokeapi.dev/joke/Any'
-  const amount = `?amount=${e.target.parentElement.childNodes[23].value}`
-
-  fetch(`${jokeServer}${amount}`)
-    .then(res => res.json())
-    .then(data => postListings(data.jokes))
-    .catch(error => alert(error.message))
+  const jokeServer = 'https://v2.jokeapi.dev/joke/'
+  const category = function() {
+    let categorySelector = Object.entries(document.getElementsByName('category'))
+    let selectedCategory = categorySelector.reduce((accum, val) => {
+      console.log(val)
+    })
+    console.log(selectedCategory)
+  }
+  // Programming,Miscellaneous,Dark,Pun,Spooky,Christmas
+  const style = function() {
+    let styleSelector = Object.entries(document.getElementsByName('style'))
+    let selectedStyle = styleSelector.find(element => element[1].checked == true)
+    if (selectedStyle[1].id === 'doublestyle') {
+      return 'type=twopart&'
+    } else if (selectedStyle[1].id === 'singlestyle') {
+      return 'type=single&'
+    } else {
+      return ''
+    }
+  }
+  const amount = `amount=${e.target.parentElement.childNodes[23].value}`
+  
+  // console.log(category())
+  // console.log(`${jokeServer}${category()}${style()}${amount}`)
+  // fetch(`${jokeServer}${category()}${style()}${amount}`)
+  //   .then(res => res.json())
+  //   .then(data => amount.charAt(7) === '1' ? postListing(data) : postListings(data.jokes))
+  //   .catch(error => alert(error.message))
 })
 
-function postListings(listings) {
-  for (const listing of listings) {
-    let category = listing.category
-    
-    if (listing.type === 'twopart') {
+function postListing(listing) {
+  console.log(listing)
+}
 
-    }
-    console.log(listing)
-  }
+function postListings(listings) {
+  console.log(listings)
+}
+
+function createJokeBlock() {
+  let jokeContainer = document.createElement('span')
+  let jokeSetup = document.createElement('h3')
+  let 
 }
