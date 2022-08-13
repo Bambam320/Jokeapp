a0ac440904c19974d71fac8fda30b8b350b52845
Igor Mestechkin <Igor.mestechkin@gmail.com>
2022-08-13 00:54:06 -0400

    updates

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
+//event listener for search, builds URL for fetch based on selection
 document.querySelector('#main-search').addEventListener('click', (e) => {
   e.preventDefault()
-  //console.log(e)
-
   const jokeServer = 'https://v2.jokeapi.dev/joke/'
   const category = function() {
-    let categorySelector = Object.entries(document.getElementsByName('category'))
-    let selectedCategory = categorySelector.reduce((accum, val) => {
-      console.log(val)
-    })
-    console.log(selectedCategory)
+    let categorySelections = Object.entries(document.getElementsByName('category'))
+    let selectedCategories = categorySelections.reduce(function(accum, val) {
+      return accum += val[1].checked === true ? `${val[1].id},` : ''
+    }, '')
+    return selectedCategories != '' ? `${selectedCategories.slice(0,-1)}?` : selectedCategories
   }
-  // Programming,Miscellaneous,Dark,Pun,Spooky,Christmas
+  const flags = function() {
+    let flagSelections = Object.entries(document.getElementsByName('flags'))
+    let selectedFlags = flagSelections.reduce(function(accum, val) {
+      return accum += val[1].checked === true ? `${val[1].id},` : ''
+    }, '')
+    return selectedFlags != '' ? `blacklistFlags=${selectedFlags.slice(0,-1)}&` : selectedFlags
+  }
   const style = function() {
     let styleSelector = Object.entries(document.getElementsByName('style'))
     let selectedStyle = styleSelector.find(element => element[1].checked == true)
@@ -22,26 +27,75 @@ document.querySelector('#main-search').addEventListener('click', (e) => {
       return ''
     }
   }
+  const searchText = document.getElementById('search-text').value
+  const searchField = `contains=${searchText.replace(/ /g, '%20')}&`
   const amount = `amount=${e.target.parentElement.childNodes[23].value}`
   
-  // console.log(category())
-  // console.log(`${jokeServer}${category()}${style()}${amount}`)
-  // fetch(`${jokeServer}${category()}${style()}${amount}`)
-  //   .then(res => res.json())
-  //   .then(data => amount.charAt(7) === '1' ? postListing(data) : postListings(data.jokes))
-  //   .catch(error => alert(error.message))
+  console.log(`${jokeServer}${category() === '' ? 'Any?' : category()}${flags()}${style()}${searchText != '' ? searchField : ''}${amount}`)
+  fetch(`${jokeServer}${category() === '' ? 'Any?' : category()}${flags()}${style()}${searchText != '' ? searchField : ''}${amount}`)
+    .then(res => res.json())
+    .then(data => amount.charAt(7) === '1' ? postListing(data) : postListings(data.jokes))
+    .catch(error => alert(error.message))
 })
 
+//posts a single listing to the page
 function postListing(listing) {
-  console.log(listing)
+  if (listing.error === true) {
+    return alert('No Matching Jokes Found')
+  } else if(listing.error === false || listing.id != NaN) {
+    jokeListBuilder(listing)
+  }
 }
 
+//posts multiple listings to the page
 function postListings(listings) {
-  console.log(listings)
+  if (listings === undefined) {
+    return alert('No Matching Jokes Found')
+  } else {
+    for (const listing of listings) {
+      postListing(listing)
+    }
+  }
 }
 
-function createJokeBlock() {
+//takes a listing and builds a joke card and adds it the page
+function jokeListBuilder(listing) {
   let jokeContainer = document.createElement('span')
-  let jokeSetup = document.createElement('h3')
-  let 
+  let joke = document.createElement('b')
+  let setup = document.createElement('b')
+  let delivery = document.createElement('b')
+  let lineBreak = document.createElement('br')
+  let category = document.createElement('p')
+  let id = document.createElement('p')
+  let flags = document.createElement('ul')
+  let saveButton = document.createElement('button')
+  let deleteButton = document.createElement('button')
+  let jokeList = document.querySelector('.listing')
+  const appliedFlags = function() {
+    return Object.entries(listing.flags).reduce(function(accum, val) {
+      return accum += val[1] === true ? `${val[0]}, ` : '' 
+    }, '')
+  }
+  jokeContainer.className = 'joke-card'
+  flags.textContent = `Flags Applied : ${appliedFlags() != '' ? appliedFlags().slice(0,-2) : 'none'}`
+  joke.textContent = listing.joke
+  setup.textContent = listing.setup
+  delivery.textContent = listing.delivery
+  category.textContent = `Category : ${listing.category}`
+  id.textContent = `Joke Id : ${listing.id}`
+  saveButton.textContent = 'Save This Joke!'
+  deleteButton.textContent = 'Delete This Joke!'
+  jokeList.appendChild(jokeContainer)
+  if (listing.type === 'single') {
+    jokeContainer.append(joke, category, id, flags, saveButton, deleteButton)
+  } else {
+    jokeContainer.append(setup, lineBreak, delivery, category, id, flags, saveButton, deleteButton)
+  }
 }
+
+//deletes all jokes from the page
+document.querySelector('#delete_all_jokes').addEventListener('click', (e) => {
+  document.querySelector('.listing').removeChild()
+})
+
+//saves a joke to the favorites folder
