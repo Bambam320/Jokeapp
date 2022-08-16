1124d1ccb61da4ec11baa869d529fcc2e924ec85
Igor Mestechkin <Igor.mestechkin@gmail.com>
2022-08-16 01:25:05 -0400

    java update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 //event listener for search, builds URL for fetch based on selection
 document.querySelector('#main-search').addEventListener('click', (e) => {
   e.preventDefault()
+  //clear jokes before displaying new ones
+  if (document.getElementById('clear_submit').checked === true) {
+    removeAll()
+  }
+  // adding joke selection information to variables
   const jokeServer = 'https://v2.jokeapi.dev/joke/'
   const category = function() {
     let categorySelections = Object.entries(document.getElementsByName('category'))
@@ -30,7 +35,7 @@ document.querySelector('#main-search').addEventListener('click', (e) => {
   const searchText = document.getElementById('search-text').value
   const searchField = `contains=${searchText.replace(/ /g, '%20')}&`
   const amount = `amount=${e.target.parentElement.childNodes[23].value}`
-  
+  //fetching the jokes from the api by interpolating the variables for joke selection
   console.log(`${jokeServer}${category() === '' ? 'Any?' : category()}${flags()}${style()}${searchText != '' ? searchField : ''}${amount}`)
   fetch(`${jokeServer}${category() === '' ? 'Any?' : category()}${flags()}${style()}${searchText != '' ? searchField : ''}${amount}`)
     .then(res => res.json())
@@ -60,6 +65,7 @@ function postListings(listings) {
 
 //takes a listing and builds a joke card and adds it the page
 function jokeListBuilder(listing) {
+  //creating new elements for posting the jokes
   let jokeContainer = document.createElement('span')
   let joke = document.createElement('b')
   let setup = document.createElement('b')
@@ -69,33 +75,146 @@ function jokeListBuilder(listing) {
   let id = document.createElement('p')
   let flags = document.createElement('ul')
   let saveButton = document.createElement('button')
+  let removeButton = document.createElement('button')
   let deleteButton = document.createElement('button')
   let jokeList = document.querySelector('.listing')
+  //applying classes and filling new elements with appropriate information
+  jokeContainer.className = 'joke-card'
   const appliedFlags = function() {
     return Object.entries(listing.flags).reduce(function(accum, val) {
       return accum += val[1] === true ? `${val[0]}, ` : '' 
     }, '')
   }
-  jokeContainer.className = 'joke-card'
   flags.textContent = `Flags Applied : ${appliedFlags() != '' ? appliedFlags().slice(0,-2) : 'none'}`
   joke.textContent = listing.joke
   setup.textContent = listing.setup
   delivery.textContent = listing.delivery
   category.textContent = `Category : ${listing.category}`
+  category.className = 'cat'
   id.textContent = `Joke Id : ${listing.id}`
+  id.id = listing.id
+  id.className = 'jokeID'
   saveButton.textContent = 'Save This Joke!'
-  deleteButton.textContent = 'Delete This Joke!'
+  saveButton.addEventListener('click', (e) => {
+    saveThisJoke(e)
+  })
+  removeButton.textContent = 'Remove This Joke!'
+  removeButton.addEventListener('click', (e) => {
+    removeThisJoke(e)
+  })
+  deleteButton.textContent = 'Delete This Joke From Favorites!'
+  deleteButton.addEventListener('click', (e) => {
+    deleteThisJoke(e)
+  })
+  //appending the elements to the main container based on joke type
   jokeList.appendChild(jokeContainer)
-  if (listing.type === 'single') {
-    jokeContainer.append(joke, category, id, flags, saveButton, deleteButton)
-  } else {
-    jokeContainer.append(setup, lineBreak, delivery, category, id, flags, saveButton, deleteButton)
+  switch(true) {
+    case listing.type === 'single' && listing.local === undefined:
+    jokeContainer.append(joke, category, id, flags, saveButton, removeButton)
+    break;
+    case listing.type === 'twopart' && listing.local === undefined:
+    jokeContainer.append(setup, lineBreak, delivery, category, id, flags, saveButton, removeButton)
+    break;
+    case listing.local === true && listing.type === 'single':
+    jokeContainer.append(joke, category, id, flags, saveButton, removeButton, deleteButton)
+    break;
+    case listing.local === true && listing.type === 'twopart':
+    jokeContainer.append(setup, lineBreak, delivery, category, id, flags, saveButton, removeButton, deleteButton)
+    break;
   }
 }
 
-//deletes all jokes from the page
-document.querySelector('#delete_all_jokes').addEventListener('click', (e) => {
-  document.querySelector('.listing').removeChild()
+//listens for remove all button and uses remove all function
+document.querySelector('#remove_all_jokes').addEventListener('click', (e) => {
+  e.preventDefault()
+  removeAll()
+}) 
+
+//removes all jokes from the page
+const removeAll = function() {
+  const container = document.querySelector('.listing')
+  while(container.firstChild) {
+    container.removeChild(container.firstChild)
+  }
+}
+
+//removes a single joke from the page
+function removeThisJoke(joke) {
+  joke.target.parentElement.remove()
+}
+
+//deletes a joke from the local server
+function deleteThisJoke(joke) {
+  fetch(`http://localhost:3000/Favorites/${joke.target.parentElement.querySelector('.jokeID').id === true ? joke.target.parentElement.querySelector('.jokeID').id : joke.id}`, {method: 'DELETE'})
+  .then(res => res.json())
+  .then(data => joke.target.parentElement.remove())
+}
+
+//iterates through jokes in favorites and passes them through function that deletes them
+document.querySelector('#delete_all_saved_jokes').addEventListener('click', (e) => {
+  e.preventDefault()
+  fetch('http://localhost:3000/Favorites')
+  .then(res => res.json())
+  .then(data => deleteAllJokes(data))
 })
 
+//deletes all jokes from the json server
+function deleteAllJokes(jokes) {
+  for (const joke of jokes) {
+    fetch(`http://localhost:3000/Favorites/${joke.id}`, {method: 'DELETE'})
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }
+}
+
 //saves a joke to the favorites folder
+function saveThisJoke(joke) {
+  const myJokesFolder = 'http://localhost:3000/Favorites'
+  const addJoke = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      'joke_id': `${joke.target.parentElement.querySelector('.jokeID').id}`,
+      'id' : `${joke.target.parentElement.querySelector('.jokeID').id}`,
+      'joke': `${joke.target.parentElement.querySelector('.cat').textContent.slice(11)}`,
+    })
+  }
+  fetch(myJokesFolder, addJoke)
+    .then(res => res.json())
+    .then(data => (data))
+    .catch(error => alert(error))
+}
+
+//Listen for favorites folder request and pass jokes to lister function
+document.querySelector('#favorite_button').addEventListener('click', (e) => {
+  e.preventDefault()
+  fetch('http://localhost:3000/Favorites')
+    .then(res => res.json())
+    .then(data => listFavorites(data))
+    .catch(error => alert(error))
+})
+
+//iterate over the jokes on the local server and request each joke from the API then pass to function for modification
+function listFavorites(jokes) {
+  for (const joke of jokes) {
+    fetch(`https://v2.jokeapi.dev/joke/Any?idRange=${joke.joke_id}`)
+    .then(res => res.json())
+    .then(data => modifyAndPostListing(data))
+  }
+}
+
+//add a key value pair to the joke then pass to postListing function which will add a delete button because its a saved joke
+function modifyAndPostListing(jokes) {
+  jokes.local = true
+  postListing(jokes)
+}
+
+
+
+//saves all jokes to the folder
+
+
+
